9fe26e1a031cb316f3e2889fc41e7102a0e1365c
lczyk <marcin.konow@lczyk.xyz>
2026-05-08 15:20:50 +0100

    fix: missing percent delimiter in kittykittymewmew fixture
    `%ts%` was transcribed from the esolangs wiki with a missing `%` --
    should be `%%ts%` (a `%`-delimited string literal).

--- internal/integrationtest/testdata/esolangs/kittykittymewmew.rb
+++ internal/integrationtest/testdata/esolangs/kittykittymewmew.rb
@@ -2,7 +2,7 @@
 # source: https://esolangs.org/wiki/KittyKittyMewMew
 # language author: User:ryanninjasheep
 # licence: CC0 (esolangs.org wiki content)
-C = {/M/ => "pu",/O/ => ' g',%r,W, => "ets",/E/=>%ts%,/.*[^OpuWtMsEge\t \n]/=>"puts 'invalid code'#"}
+C = {/M/ => "pu",/O/ => ' g',%r,W, => "ets",/E/=>%%ts%,/.*[^OpuWtMsEge\t \n]/=>"puts 'invalid code'#"}
 def run_code code
   eval(C.to_a.inject(code){|x,y| x.gsub y[0],y[1]})
 end
