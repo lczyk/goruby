503a70cd98d97b0e7c7403b2ae07329b7d1ed1c1
lczyk <marcin.konow@lczyk.xyz>
2026-05-09 16:15:02 +0100

    chore: gofmt lexer/lexer.go

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -65,17 +65,17 @@ func New(input string, opts ...Option) *Lexer {
 
 // Lexer is the engine to process input and emit Tokens
 type Lexer struct {
-	input         string           // the string being scanned.
-	state         StateFn          // the next lexing function to enter
-	pos           int              // current position in the input.
-	start         int              // start position of this item.
-	width         int              // width of last rune read from input.
-	tokens        chan token.Token // channel of scanned tokens.
-	lastToken     token.Token      // lastToken stores the last token emitted by the lexer
-	hadWhitespace bool             // true if whitespace was skipped before current token
-	ternaryDepth        int  // pending ternary ? without matching :
-	version             token.RubyVersion
-	tokenHadWhitespace  bool // whitespace before the token currently being lexed
+	input              string           // the string being scanned.
+	state              StateFn          // the next lexing function to enter
+	pos                int              // current position in the input.
+	start              int              // start position of this item.
+	width              int              // width of last rune read from input.
+	tokens             chan token.Token // channel of scanned tokens.
+	lastToken          token.Token      // lastToken stores the last token emitted by the lexer
+	hadWhitespace      bool             // true if whitespace was skipped before current token
+	ternaryDepth       int              // pending ternary ? without matching :
+	version            token.RubyVersion
+	tokenHadWhitespace bool // whitespace before the token currently being lexed
 
 	// Heredoc state.
 	heredocDelim    string
